7b1ebc8622a4f10c1d306c5d1d8b244a9dba7ba5
Demix88 <skrasnauskis@gmail.com>
2022-03-23 20:55:49 +0000

    Add project files.

---
+++ InheritanceAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAssignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var Person=new Person();
+            
+            Person.SayName();
+
+           var Employee = new Employee();
+            
+            Employee.FirstName = "Student";
+            Employee.LastName = "Sample";
+                Employee.SayName();
+            
+            {
+         Console.ReadLine();
+
+            }
+        }
+    }
+}
